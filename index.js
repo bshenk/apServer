1cb526846f3fc60207ddede9ac36c1c68e347fb4
Brandon Shenk <brandon.shenk@axonai.com>
2017-12-20 10:21:36 -0500

    fixed typo

--- index.js
+++ index.js
@@ -160,7 +160,7 @@ app.post('/export', (req, res) => {
 })
 
 const options = {
-  cert: fs.readFileSync('/etc/letsencrypt/live/dev.patent.axonai.com/fulllchain.pem'),
+  cert: fs.readFileSync('/etc/letsencrypt/live/dev.patent.axonai.com/fullchain.pem'),
   key: fs.readFileSync('/etc/letsencypt/live/dev.patent.axonai.com/privkey.pem')
 }
 
